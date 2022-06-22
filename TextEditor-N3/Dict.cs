43163976ba0f70cb35a7c8150e9bc2c178f0a4f4
Nathan <nathan.ac@outlook.com>
2022-06-22 17:57:49 -0300

    v4

--- TextEditor-N3/Dict.cs
+++ TextEditor-N3/Dict.cs
@@ -9,11 +9,12 @@ namespace TextEditor_N3
 {
     public partial class Dict : Form
     {
+        //Variáveis globais declaradas para serem utilizadas em quantos processos forem necessários.
         string dictPath = "dict.txt";
 
         char[] separators = new char[]
         {
-            ' ', '\r', '\n', ',', '\t'
+            ' ', '\r', '\n', ',', '\t', ';'
         };
 
         public Dict()
@@ -23,6 +24,7 @@ namespace TextEditor_N3
 
         public void RenewGrid(string[] dados)
         {
+            //Popular dados - dinâmico para atualizar de acordo com as atuações de adicionar/remover.
             for (int i = 0; i < dados.Length; i++)
             {
                 dataGridView1.Rows.Add(dados[i]);
@@ -31,37 +33,37 @@ namespace TextEditor_N3
 
         public void ShowGrid(string[] dados)
         {
+            //Criar coluna Palavras do grid.
             dataGridView1.Columns.Add("word", "Palavras");
-
             RenewGrid(dados);
         }
 
         private void removerToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
-            if (dataGridView1.SelectedCells.Count > 0)
+            if (dataGridView1.SelectedCells.Count > 0) //Linha selecionada
             {
-                if (dataGridView1.SelectedCells.Count == 1)
+                if (dataGridView1.SelectedCells.Count == 1) // Execução única.
                 {
-                    int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
-                    DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
-                    string cellValue = Convert.ToString(selectedRow.Cells["word"].Value).ToLower();
+                    int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex; //Definindo indice da linha selecionada
+                    DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex]; // Definindo linha a ser selecionada
+                    string cellValue = Convert.ToString(selectedRow.Cells["word"].Value).ToLower(); //Obtendo valor da linha selecionada
 
-                    if (cellValue != "" && cellValue != null)
+                    if (cellValue != "" && cellValue != null) //Verificando se o valor da linha não é nulo.
                     {
-                        string dictContent = File.ReadAllText(dictPath);
-                        string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                        string dictContent = File.ReadAllText(dictPath); //Conteúdo do dicionário
+                        string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Splitando conteúdo do dicionário para verificar a nível de palavra.
 
-                        string formCell = $@"\b{cellValue}\b";
+                        string formCell = $@"\b{cellValue}\b"; //Formatação para obter palavra exata.
 
-                        var newLines = dictWords.Select(line => Regex.Replace(line, formCell, string.Empty, RegexOptions.None));
-                        File.WriteAllLines(dictPath, newLines);
+                        var newLines = dictWords.Select(line => Regex.Replace(line, formCell, string.Empty, RegexOptions.None)); //Determinando novas linhas do arquivo, retirando a selecionada.
+                        File.WriteAllLines(dictPath, newLines); //Gravando novo conteúdo do dicionário.
 
-                        string newDictContent = File.ReadAllText(dictPath);
-                        string[] newDictWords = newDictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                        string newDictContent = File.ReadAllText(dictPath); //Lendo novo conteúdo do dicionário.
+                        string[] newDictWords = newDictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Splitando novo conteúdo do dicionário para atualizar na tela.
 
-                        dataGridView1.Rows.Clear();
-                        RenewGrid(newDictWords);
+                        dataGridView1.Rows.Clear(); //Limpando linhas do grid.
+                        RenewGrid(newDictWords); //Populando grid.
 
                         MessageBox.Show($"Palavra {cellValue} removida com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
@@ -70,31 +72,30 @@ namespace TextEditor_N3
                         MessageBox.Show($"Selecione um valor válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
-                else
+                else // Execução múltipla.
                 {
                     int selectedRows = dataGridView1.SelectedCells.Count;
 
                     for (int i = 0; i <= selectedRows; i++)
                     {
-                        DataGridViewRow selectedRow = dataGridView1.Rows[0];
-                        string cellValue = Convert.ToString(selectedRow.Cells["word"].Value).ToLower();
+                        DataGridViewRow selectedRow = dataGridView1.Rows[0]; // Definindo linha a ser selecionada
+                        string cellValue = Convert.ToString(selectedRow.Cells["word"].Value).ToLower(); //Obtendo valor da linha selecionada
 
-                        if (cellValue != "" && cellValue != null)
+                        if (cellValue != "" && cellValue != null) //Verificando se o valor da linha não é nulo.
                         {
-                            string dictContent = File.ReadAllText(dictPath);
-                            string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                            string dictContent = File.ReadAllText(dictPath); //Conteúdo do dicionário
+                            string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Splitando conteúdo do dicionário para verificar a nível de palavra.
 
-                            string formCell = $@"\b{cellValue}\b";
+                            string formCell = $@"\b{cellValue}\b"; //Formatação para obter palavra exata.
 
-                            var newLines = dictWords.Select(line => Regex.Replace(line, formCell, string.Empty, RegexOptions.None));
-                            File.WriteAllLines(dictPath, newLines);
+                            var newLines = dictWords.Select(line => Regex.Replace(line, formCell, string.Empty, RegexOptions.None)); //Determinando novas linhas do arquivo, retirando a selecionada.
+                            File.WriteAllLines(dictPath, newLines); //Gravando novo conteúdo do dicionário.
 
-                            string newDictContent = File.ReadAllText(dictPath);
-                            string[] newDictWords = newDictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                            string newDictContent = File.ReadAllText(dictPath); //Lendo novo conteúdo do dicionário.
+                            string[] newDictWords = newDictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Splitando novo conteúdo do dicionário para atualizar na tela.
 
-                            dataGridView1.Rows.Clear();
-                            dataGridView1.Refresh();
-                            RenewGrid(newDictWords);
+                            dataGridView1.Rows.Clear(); //Limpando linhas do grid.
+                            RenewGrid(newDictWords); //Populando grid.
                         }
                     }
                     MessageBox.Show("Todas as palavras selecionadas foram removidas com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -110,20 +111,19 @@ namespace TextEditor_N3
         {
             if (dataGridView1.SelectedCells.Count > 0)
             {
-                int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
-                DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
-                string wordDict = Convert.ToString(selectedRow.Cells["word"].Value).ToLower();
+                int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex; //Definindo indice da linha selecionada
+                DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex]; // Definindo linha a ser selecionada
+                string wordDict = Convert.ToString(selectedRow.Cells["word"].Value).ToLower(); //Obtendo valor da linha selecionada
 
-                if (wordDict != "" && wordDict != null)
+                if (wordDict != "" && wordDict != null) //Verificando se o valor da linha não é nulo.
                 {
-                    File.AppendAllText(dictPath, wordDict + Environment.NewLine);
+                    File.AppendAllText(dictPath, wordDict + Environment.NewLine); //Adicionando nova linha no arquivo do dicionário
                     MessageBox.Show($"Palavra {wordDict} adicionada ao dicionário.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
                     MessageBox.Show($"Selecione um valor válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
             }
             else
             {
--- TextEditor-N3/Form1.cs
+++ TextEditor-N3/Form1.cs
@@ -9,6 +9,7 @@ namespace TextEditor_N3
 {
     public partial class Form1 : Form
     {
+        //Variáveis globais declaradas para serem utilizadas em quantos processos forem necessários.
         string dictPath = "dict.txt";
         int startIndex = 0;
         char[] separators = new char[]
@@ -23,24 +24,25 @@ namespace TextEditor_N3
 
         private void DictCheck()
         {
+            //Tratativa para atualizar uma palavra adicionada no dicionário.
             rtbEditor.SelectAll();
             rtbEditor.SelectionBackColor = Color.Transparent;
             rtbEditor.DeselectAll();
 
-            string dictContent = File.ReadAllText(dictPath);
-            string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            string[] editorWords = rtbEditor.Text.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string dictContent = File.ReadAllText(dictPath); // Ler conteúdo do dicionário
+            string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries); // Obter palavras do dicionário
+            string[] editorWords = rtbEditor.Text.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Obter palavras do editor de texto.
 
             for (int i = 0; i < editorWords.Length; i++)
             {
-                if (dictWords.Contains(editorWords[i])) { }
+                if (dictWords.Contains(editorWords[i])) { } // Se a palavra estiver contida no dicionário, não faça nada.
                 else
                 {
-                    while (startIndex < rtbEditor.TextLength)
+                    while (startIndex < rtbEditor.TextLength) //Tratativa para verificar sempre do começo do texto
                     {
-                        int wordStartIndex = rtbEditor.Find(editorWords[i], startIndex, RichTextBoxFinds.WholeWord);
+                        int wordStartIndex = rtbEditor.Find(editorWords[i], startIndex, RichTextBoxFinds.WholeWord); //Index que a palavra se inicia
 
-                        if (wordStartIndex != -1)
+                        if (wordStartIndex != -1) // Se = -1, significa que a palavra naõ está no dicionário. Marcação será feita.
                         {
                             rtbEditor.SelectionStart = wordStartIndex;
                             rtbEditor.SelectionLength = editorWords[i].Length;
@@ -55,33 +57,35 @@ namespace TextEditor_N3
                     }
                 }
             }
+            //Tratativa para melhorar a experiência do usuário e remover a seleção feita na marcação.
             rtbEditor.DeselectAll();
             startIndex = 0;
         }
 
         private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.Filter = "Text File (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
-            if (dlg.ShowDialog() == DialogResult.OK)
+            OpenFileDialog dlg = new OpenFileDialog(); //Usando dialog de arquivos
+            dlg.Filter = "Text File (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*"; //Filtro txt e rtf
+            if (dlg.ShowDialog() == DialogResult.OK) // Se o diálogo ocorrer com sucesso
             {
-                rtbEditor.Text = File.ReadAllText(dlg.FileName);
+                rtbEditor.Text = File.ReadAllText(dlg.FileName); // Editor de texto recebe todo o texto do arquivo aberto.
             }
         }
 
         private void adicionarToolStripMenuItem1_Click(object sender, EventArgs e)
         {
 
-            if (rtbEditor.Text.Length == 0)
+            if (rtbEditor.Text.Length == 0) //Se o editor não tiver nada escrito/nenhum arquivo aberto.
             {
                 MessageBox.Show("Texto vazio. Favor preencher e selecionar a palavra que deseja adicionar.", "Erro");
             }
-            else if (rtbEditor.SelectionLength == 0)
+            else if (rtbEditor.SelectionLength == 0) //Se nada foi selecionado
             {
                 MessageBox.Show("Favor selecionar a palavra que deseja adicionar.", "Erro");
             }
             else
             {
+                //Obtendo palavra selecionada pelo usuário e adicionando no dicionário.
                 rtbEditor.Select(rtbEditor.SelectionStart, rtbEditor.SelectionLength);
                 string wordDict = rtbEditor.SelectedText;
                 File.AppendAllText(dictPath, wordDict + Environment.NewLine);
@@ -91,89 +95,95 @@ namespace TextEditor_N3
 
         private void removerToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
-            if (rtbEditor.Text.Length == 0)
+            if (rtbEditor.Text.Length == 0) //Se o editor não tiver nada escrito/nenhum arquivo aberto.
             {
                 MessageBox.Show("Texto vazio. Favor preencher e selecionar a palavra que deseja adicionar.", "Erro");
             }
-            else if (rtbEditor.SelectionLength == 0)
+            else if (rtbEditor.SelectionLength == 0) //Se nada foi selecionado
             {
                 MessageBox.Show("Favor selecionar a palavra que deseja adicionar.", "Erro");
             }
             else
             {
+                //Obtendo conteúdo do dicionário
                 string dictContent = File.ReadAllText(dictPath);
                 string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
+                //Obtendo palavra selecionada pelo usuário e removendo do dicionário.
                 rtbEditor.Select(rtbEditor.SelectionStart, rtbEditor.SelectionLength);
                 string wordDict = rtbEditor.SelectedText;
-                var newLines = dictWords.Select(line => Regex.Replace(line, wordDict, string.Empty, RegexOptions.IgnoreCase));
+                string formWord = $@"\b{wordDict}\b";
+                var newLines = dictWords.Select(line => Regex.Replace(line, formWord, string.Empty, RegexOptions.IgnoreCase));
                 File.WriteAllLines(dictPath, newLines);
+                MessageBox.Show($"Palavra {wordDict} removida do dicionário.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
         private void verificarToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Verificação do dicionário
             DictCheck();
         }
 
         private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "Text File (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
-            sfd.Title = "Salve seu arquivo.";
+            SaveFileDialog sfd = new SaveFileDialog(); //Abrindo diálogo para escolhar local do arquivo
+            sfd.Filter = "Text File (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*"; //Definindo filtro de extensões.
+            sfd.Title = "Salve seu arquivo."; //Título do diálogo.
 
-            if (sfd.ShowDialog() == DialogResult.OK)
+            if (sfd.ShowDialog() == DialogResult.OK) // Se o diálogo ocorrer com sucesso
             {
-                FileStream fs = new FileStream(sfd.FileName, FileMode.Create);
-                StreamWriter sw = new StreamWriter(fs);
-                sw.Write(rtbEditor.Text);
-                sw.Close();
+                FileStream fs = new FileStream(sfd.FileName, FileMode.Create); //Acessar o caminho em modo de criação
+                StreamWriter sw = new StreamWriter(fs); //Abrir um escritor para determinar o conteúdo do arquivo
+                sw.Write(rtbEditor.Text); //Escrever os dados
+                sw.Close(); //Fechar arquivo
                 MessageBox.Show($"Arquivo salvo com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
         private void abrirDictToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string dictContent = File.ReadAllText(dictPath);
-            string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string dictContent = File.ReadAllText(dictPath); //Obter conteúdo do dicionário
+            string[] dictWords = dictContent.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Obter palavras do dicionário
 
-            Dict d1 = new Dict();
-            d1.ShowGrid(dictWords);
-            d1.Show();
+            Dict d1 = new Dict(); //Instanciando novo form (grid do dicionário)
+            d1.ShowGrid(dictWords); //Chamando função ShowGrid com parâmetro de palavras
+            d1.Show(); //Chamando novo form.
         }
 
         private void desfazerToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            rtbEditor.Undo();
+            rtbEditor.Undo(); //Desfazer
         }
 
         private void refazerToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            rtbEditor.Redo();
+            rtbEditor.Redo(); //Refazer
         }
 
         private void recortarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            rtbEditor.Cut();
+            rtbEditor.Cut(); //Recortar
         }
 
         private void copiarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            rtbEditor.Copy();
+            rtbEditor.Copy(); //Copiar
         }
 
         private void colarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            rtbEditor.Paste();
+            rtbEditor.Paste(); //Colar
         }
 
         private void selecionarTudoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            rtbEditor.SelectAll();
+            rtbEditor.SelectAll(); //Selecionar todo o texto.
         }
 
         private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Sobre o aplicativo.
             MessageBox.Show("Editor de texto desenvolvido em C# que utiliza dicionário de palavras.", "Desenvolvido por: Nathan / Marcos", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
